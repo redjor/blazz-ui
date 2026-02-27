93ca215b553872818d45be92de15f407be8a879f
redjor <ruasjonathan@gmail.com>
2026-02-27 19:11:54 +0100

    fix(docs): adjust sidebar positioning for new flex layout

--- apps/docs/src/components/docs/components-sidebar.tsx
+++ apps/docs/src/components/docs/components-sidebar.tsx
@@ -35,7 +35,7 @@ export function ComponentsSidebar() {
 	}
 
 	return (
-		<aside className="sticky top-14 hidden h-[calc(100vh-3.5rem)] w-64 shrink-0 border-r bg-surface lg:block">
+		<aside className="hidden h-full w-64 shrink-0 overflow-y-auto border-r bg-surface lg:block">
 			<ScrollArea className="h-full py-6">
 				<div className="px-4 pb-4">
 					<Link
